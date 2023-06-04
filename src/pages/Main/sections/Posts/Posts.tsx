5fd5dfcdb4d05781eecc0d6e5eaaf8d6e076ccfe
Sedrak Adoyan <set.adoyan@gmail.com>
2023-06-04 21:59:27 +0400

    feat: add pagination

--- src/pages/Main/sections/Posts/Posts.tsx
+++ src/pages/Main/sections/Posts/Posts.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 
-import { Container } from 'react-bootstrap'
+import { Container, Pagination } from 'react-bootstrap'
 
 import { PostCard } from 'features'
 
@@ -8,24 +8,48 @@ import { useAppDispatch, useAppSelector } from 'store/hooks'
 
 import { fetchPosts } from 'store/slices/post/postActions'
 
+import s from './posts.module.scss'
+
 const Posts = () => {
   const { limit, page, count, posts } = useAppSelector(
     state => state.posts
   )
   const dispatch = useAppDispatch()
 
+  const getPosts = (limit: number, page: number) =>
+    dispatch(
+      fetchPosts({
+        limit,
+        page,
+      })
+    )
+
+  const paginationItems = []
+
+  for (let i = 0; i <= Math.round(count / limit) - 1; i++) {
+    paginationItems.push(
+      <Pagination.Item
+        key={i}
+        active={page === i}
+        onClick={() => getPosts(limit, i)}
+      >
+        {i + 1}
+      </Pagination.Item>
+    )
+  }
+
   useEffect(() => {
-    fetchPosts({
-      limit,
-      page,
-    })
+    getPosts(limit, page)
   }, [])
 
   return (
     <Container>
-      {posts?.map(p => (
-        <PostCard key={p.id} {...p} />
-      ))}
+      <div className={s.postsWrapper}>
+        {posts?.map(p => (
+          <PostCard key={p.id} {...p} />
+        ))}
+      </div>
+      <Pagination>{paginationItems}</Pagination>
     </Container>
   )
 }
